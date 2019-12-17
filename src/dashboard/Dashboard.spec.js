da0afdc88b4faeafc252ef1e1aa8c5cf984756b4
Matthew Cebenka <matthewt.cebenka@gmail.com>
2019-12-17 17:32:40 -0500

    MVP

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,15 @@
 // Test away
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+
+import Dashboard from "./Dashboard";
+
+test("Dashboard is there", () => {
+  const state = true;
+
+  const { baseElment, rerender } = render(<Dashboard state={state} />);
+
+  expect(baseElment).toMatchSnapshot();
+});
+
+
